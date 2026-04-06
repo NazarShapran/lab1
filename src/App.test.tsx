b030f90d41c17e8d8f2fdfb67c5f8e290d65193d
nazarSHP99 <145098692+nazarSHP99@users.noreply.github.com>
2026-04-06 22:26:13 +0300

    fix: remove unused screen import in test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App'; // Або інший твій головний компонент
 import { describe, expect, it } from 'vitest';
 
